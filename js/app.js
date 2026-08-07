8c1553fdc27c2342538a7c0cc5b546aa93ec648f
DimasAgung98 <josebatariel@gmail.com>
2026-08-07 20:55:55 +0800

    Fix PWA installation tags in index.html and app.js

--- js/app.js
+++ js/app.js
@@ -91,4 +91,13 @@ window.app = window.app || {};
 
 document.addEventListener('DOMContentLoaded', () => {
     App.init();
+
+    // Register PWA Service Worker
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js').then(reg => {
+            console.log('PWA Service Worker registered!', reg.scope);
+        }).catch(err => {
+            console.error('PWA Service Worker registration failed: ', err);
+        });
+    }
 });
